3a7423f0d77bbca4cd69c64873562f5a9cc45cd3
Vincent Bichat <vincent.bichat@epitech.eu>
2024-04-23 21:50:18 +0200

    [ADD] Updating message

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,7 @@ function checkIfMobile() {
     if (window.innerWidth < maxWidthForMobile) { // If the screen is smaller than 768px
         Swal.fire({
             title: "Be careful...",
-            text: "You should use a laptop to visualize this website correctly!",
+            text: "You should use a laptop to visualize this website correctly! (or use at least the desktop mode on Chrome)",
             icon: "info"
           });
     }
